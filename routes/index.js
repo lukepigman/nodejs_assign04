da3a036f3543743e3c613524f554e78b5c52e15a
lukepigman <ldpigma@ilstu.edu>
2019-03-15 20:21:55 -0500

    part one

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator/check');
 router.get('/', (req, res) => {
-res.send('It works!');
-});
+    res.render('form', { title: 'Registration form' });
+    });      
+
+router.post(
+    '/',
+    (req, res) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        res.send('Thank you for your registration!');
+    } else {
+        res.render('form', {
+        title: 'Registration form',
+        errors: errors.array(),
+        data: req.body,
+        });
+    }
+    }
+    );
+        
+        
+                    
 module.exports = router;
